e6610bedf73622b78acef88c22b205a4c15167ba
Marcus P <marcus.pettersson.2117@student.uu.se>
2025-04-05 15:09:12 +0200

    feat(indexing): recursive dir indexing and local reverse mapping

--- main.go
+++ main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"indexer/document"
+	"indexer/timing"
 	"log"
 )
 
 func main() {
 
-	d, err := document.DocumentFromFile("text.txt")
+	t := timing.Mesure("Main")
 
+	docs, err := document.DocumentsFromDir("test_data")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	d.GetWordsSorted()
+	// for _, doc := range docs {
+	// 	doc.DebugPrint()
+	// }
+
+	rm := document.ReverseMapping(&docs)
+
+	words := len(rm)
+
+	log.Printf("Words: %d\n", words)
+	// for word, paths := range rm {
+	// 	log.Printf("Word: %s, Paths: %v\n", word, paths)
+	// }
 
 	// for _, p := range pairs {
 	// 	fmt.Printf("%s: %d\n", p.Word, p.Freq)
 	// }
+
+	t.Stop()
 }
