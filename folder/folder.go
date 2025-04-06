1e998433b5d55037b4cd99768530a4f0c177fca8
Marcus P <marcus.pettersson.2117@student.uu.se>
2025-04-06 12:09:19 +0200

    improve(indexing): configrable folder

--- folder/folder.go
+++ folder/folder.go
@@ -11,17 +11,35 @@ type Folder struct {
 	docs docMap
 }
 
+// FolderConfig is a configuration struct for the folder
+type FolderConfig struct {
+	walkDirConfig *utils.WalkDirConfig
+}
+
+// NewFolderConfig creates a new FolderConfig with default values
+func NewFolderConfig() *FolderConfig {
+	return &FolderConfig{
+		walkDirConfig: utils.NewWalkDirConfig(),
+	}
+}
+
+func FolderConfigFromDir(walkDirConfig *utils.WalkDirConfig) *FolderConfig {
+	return &FolderConfig{
+		walkDirConfig: walkDirConfig,
+	}
+}
+
 // The folder struct will start as a singleton, but later expanded such that we can multiple folders to sort documents into groups
 
 // Type alias
 type docMap map[string]document.Document
 
-func FolderFromDir(path string) (Folder, error) {
+func (c *FolderConfig) FolderFromDir(path string) (Folder, error) {
 
 	t := timing.Mesure("FolderFromDir")
 	defer t.Stop()
 
-	docs, err := docMapFromDir(path)
+	docs, err := c.docMapFromDir(path)
 
 	if err != nil {
 		return Folder{}, err
@@ -30,13 +48,11 @@ func FolderFromDir(path string) (Folder, error) {
 	return Folder{docs: docs}, nil
 }
 
-func docMapFromDir(path string) (docMap, error) {
+func (c *FolderConfig) docMapFromDir(path string) (docMap, error) {
 
 	docs := make(docMap)
 
-	c := utils.NewWalkDirConfig().SetAllowedExts([]string{".txt", ".md", ".json", ".xml", ".html", "htm", ".xhtml", ".csv"})
-
-	for path := range c.WalkDir(path) {
+	for path := range c.walkDirConfig.WalkDir(path) {
 		doc, err := document.DocumentFromFile(path)
 		if err != nil {
 			return nil, err
