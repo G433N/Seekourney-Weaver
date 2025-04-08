6e38c667ca40b7653f7d7ece5d13bb0d4110c716
ASennblad <alexandra.sennblad@gmail.com>
2025-04-08 23:29:30 +0200

    feat(+page.svelte): very simple base for website

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    interface SearchResult {
+        title: string;
+        desc: string;   /*fix how interface should look to correspond with backend*/
+    }
+
+    let query: string = '';
+    let results: SearchResult[] = [];
+
+    async function search(): Promise<void> {
+        const res = await fetch(`http://localhost:8080/search?q=${query}`); /*change to url we are using for backend*/
+        results = await res.json() as SearchResult[];
+    }
+
+</script>
+
+<h1>Seekourny Weaver</h1>
+
+<input bind:value={query} type="text" placeholder="Write your search here!">
+<button on:click={search}> Search </button>
+
+{#if results.length > 0}
+    <ul>
+        {#each results as res}
+            <li> {res.title} </li>
+        {/each}
+    </ul>
+{/if}
