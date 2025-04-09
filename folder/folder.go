4a963806eb9ca46202c7133d8160fb3ce5a6843e
albin-uu <albin.lord.4207@student.uu.se>
2025-04-09 19:41:07 +0200

    feat(back): folder add/remove doc

--- folder/folder.go
+++ folder/folder.go
@@ -50,6 +50,19 @@ func FromIter(normalize normalize.Normalizer, docs iter.Seq2[utils.Path, documen
 	return folder
 }
 
+// Adds a document to the folder
+func (folder *Folder) AddDoc(path utils.Path, doc document.Document) {
+	folder.docs[path] = doc
+}
+
+// Removes a document from the folder
+// Returns the document (if it was removed) and bool indicating if it was removed
+func (folder *Folder) RemoveDoc(path utils.Path) (document.Document, bool) {
+	doc, ok := folder.GetDoc(path)
+	delete(folder.docs, path) // Does nothing if entry does not exist.
+	return doc, ok
+}
+
 // Creates a reverse mapping of the documents in the folder, words to paths for fast searching
 func (folder *Folder) ReverseMappingLocal() utils.ReverseMap {
 	// TODO: Use a database for this in the future
