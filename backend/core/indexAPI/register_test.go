bba7549e673f60f38d541c2c648cb68ab0b556de
Albin-uu <albin.lord.4207@student.uu.se>
2025-04-24 18:31:43 +0200

    test(back): index registration improvements

--- backend/core/indexAPI/register_test.go
+++ backend/core/indexAPI/register_test.go
@@ -7,6 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Temp until we stop using global vars
+func resetRegistration() {
+	registeredIndexers = make(map[RegisterID]IndexerInfo)
+	indexersForFileType = make(map[utils.FileType][]RegisterID)
+}
+
 func TestIsValidPort(t *testing.T) {
 	assert.False(t, isValidPort(42))
 	assert.False(t, isValidPort(MININDEXERPORT-1))
@@ -21,6 +27,7 @@ func TestIsValidPort(t *testing.T) {
 // TODO isUnoccupied and with register
 
 func TestRegisterIndexer(t *testing.T) {
+	resetRegistration()
 	firstIndexerID, err := RegisterIndexer(
 		"The text indexer",
 		"/home/theorganisation/indexers/text",
@@ -36,12 +43,13 @@ func TestRegisterIndexer(t *testing.T) {
 		"go run main.go",
 		[]utils.FileType{"md", "txt", "csv"},
 		39498)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	assert.NotEqual(t, firstIndexerID, secondIndexerID)
 }
 
 func TestRegisterIndexerInvalidPort(t *testing.T) {
+	resetRegistration()
 	const invalidPort = 5000
 	_, err := RegisterIndexer(
 		"The Best indexer",
@@ -50,10 +58,11 @@ func TestRegisterIndexerInvalidPort(t *testing.T) {
 		[]utils.FileType{"txt"},
 		invalidPort,
 	)
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestUnregisterIndexer(t *testing.T) {
+	resetRegistration()
 	firstIndexerID, _ := RegisterIndexer(
 		"The text indexer",
 		"/home/textindex/textindexer/program.exe",
@@ -68,8 +77,8 @@ func TestUnregisterIndexer(t *testing.T) {
 		[]utils.FileType{"md", "txt", "csv"},
 		39498)
 
-	assert.Nil(t, UnregisterIndexer(firstIndexerID))
-	assert.NotNil(t, UnregisterIndexer(firstIndexerID))
+	assert.NoError(t, UnregisterIndexer(firstIndexerID))
+	assert.Error(t, UnregisterIndexer(firstIndexerID))
 
-	assert.Nil(t, UnregisterIndexer(secondIndexerID))
+	assert.NoError(t, UnregisterIndexer(secondIndexerID))
 }
