9365ca9ef8a5a6d1f3f9522462a2141230f4ea79
Albin-uu <albin.lord.4207@student.uu.se>
2025-05-08 14:34:49 +0200

    refactor(core): DispatchError logging

--- backend/core/server/server.go
+++ backend/core/server/server.go
@@ -417,25 +417,7 @@ func handlePushCollection(
 		errs := indexers.DispatchFromCollection(serverParams.db, collection)
 		indexers.Mutex.Unlock()
 
-		if errs.IndexerWasRunning {
-			log.Print("Indexer was running when Core made dispatch attempt")
-		} else {
-			log.Print(
-				"Indexer needed to be started when Core made dispatch attempt")
-		}
-		if errs.StartupAttempt != nil {
-			log.Print("Dispatch attempt failed to start up indexer " +
-				"dispatch attempt aborted, failed with error: " +
-				errs.StartupAttempt.Error())
-		}
-		if errs.DispatchAttempt != nil {
-			log.Print(
-				"Indexer was not able to handle indexing dispatch request" +
-					", failed with error: " + errs.DispatchAttempt.Error())
-		} else {
-			log.Print("Pushed new collection to system and " +
-				"successfully dispatched it to indexer it")
-		}
+		logDispatchErrors(errs)
 	}()
 }
 
