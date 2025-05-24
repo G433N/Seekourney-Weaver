b4a8abc0b7bf672f31fd794e8d0375f52f9a7f2f
ASennblad <alexandra.sennblad@gmail.com>
2025-05-24 17:19:12 +0200

    feat(front): added functionallity for collections and some style changes

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -22,9 +22,19 @@
 		Port: number;
 	}
 
-	let indexerInput: '';
+	
+
+	let indexerInput: string = $state('');
 	// let submittedIndexer: string = '';
 	//let indexerList: IndexerResult[] = $state([]);
+
+	let collectionPath: string = $state('');
+	let sourceDropdownOpen: boolean = $state(false);
+	let selectedCollections: string = $state('Select type');
+	const optionsCollections: string[] = ['File', 'Folder', 'Webpage'];
+	let selectedIndexerID: string = $state('Select an indexer');
+	let indexerDropdownOpen: boolean = $state(false);
+	
 
 	async function fetchIndexers(): Promise<void> {
 		try {
@@ -64,6 +74,57 @@
 		// TODO: unsure if we get a response?
 	}
 	*/
+
+	async function addCollection(): Promise<void> {
+		const sourceMap = {
+			'File': 0,
+			'Folder': 1, 
+			'Webpage': 2
+		} as const;
+
+		let sourceType: number = sourceMap[selectedCollections as keyof typeof sourceMap] ?? -1;
+		
+		if (!collectionPath || !selectedIndexerID || sourceType === -1) {
+			console.error("missing required fields");
+			return;
+		}
+
+		const payload = {
+			Path: collectionPath,
+			IndexerID: selectedIndexerID,
+			SourceType: sourceType,
+			Recursive: true,
+			RespectLastModified: false,
+			NormalFunc: 0
+		};
+
+		try {
+			await fetch('http://localhost8080/push/collection', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(payload)
+			});
+
+			collectionPath = '';
+			selectedIndexerID = '';
+			selectedCollections = 'Select a type';
+			selectedIndexerID = 'Select an indexer';
+		} catch (err) {
+			console.error('Failed to add collection', err);
+		}
+	}
+
+	function selectOption(option: string) {
+		selectedCollections = option;
+		sourceDropdownOpen = false;
+	}
+
+	function selectIndexer(id: string) {
+		selectedIndexerID = id.toString();
+		indexerDropdownOpen = false;
+	}
 
 	onMount(() => {
 		fetchIndexers();
@@ -122,19 +183,19 @@
 	</div>
 
 	<h2>
-		Indexer
+		Indexing
 		<span class="tooltip-wrapper">
 			<span class="info-icon">?</span>
 			<span class="tooltip-text">
-				An indexer is a small program that looks through files or websites and creates a list of what they contain
-				, like words in documents, so you can search and find things quickly.
+				An indexer is a small program that looks through files or websites and creates a list of what they contain,
+				 like words in documents, so you can search and find things quickly.
 				<br>
 				<br>
 				How to use:
 				<br>
 				1. Enter the absolute path in the search field, the indexer must be located among your files.
 				<br>
-				2. Click "Add" - this will register you indexer and start using it automatically
+				2. Click "Add" - this will register your indexer and start using it automatically
 				<br>
 				<br>
 				You can add multiple indexers to handle different kinds of data.
@@ -142,16 +203,16 @@
 		</span>
 	</h2>
 	<div class="box column">
-		<label class="max-label">
+		<div class="indexer-div">
 			<p>
 				You can add your own custom indexer by providing the absolute path to a local executable on your device.
 			</p>
 			<div>
 				Indexer path:
 				<input id="InputIndexer" type="text" bind:value={indexerInput} placeholder="/user/example/custom-indexer" />
-				<button id="indexerButton" onclick={() => addIndexer()}> Add </button>
+				<button class="indexerButton" onclick={() => addIndexer()}> Add </button>
 			</div>
-		</label>
+		</div>
 
 		{#if $indexerList.length > 0}
 			{#each $indexerList as indexer}
@@ -168,6 +229,59 @@
 				</div>
 			{/each}
 		{/if}
+	</div>
+
+	<div class="box column">
+		<div class="indexer-div">
+			<p>
+				You can add a new file, folder or webpage to search through by providing the absolute path or URL to it, 
+				selecting the type and selecting what indexer to use.
+
+			</p>
+			
+			<div id="collectionDropdowns">
+				<div class="dropdown">
+					<button class="dropdownToggle" onclick={() => sourceDropdownOpen = !sourceDropdownOpen}>
+						{selectedCollections}
+					</button>
+					{#if sourceDropdownOpen}
+						<ul class="menu">
+							{#each optionsCollections as option}
+								<li>
+									<button onclick={() => selectOption(option)}>
+										{option}
+									</button>
+								</li>
+							{/each}
+						</ul>
+					{/if}
+				</div>
+
+				<div class="dropdown"> 
+					<button class="dropdownToggle" onclick={() => indexerDropdownOpen = !indexerDropdownOpen}>
+						{selectedIndexerID}
+					</button>
+					{#if indexerDropdownOpen}
+						<ul class="menu">
+							{#each $indexerList as indexer}
+								<li>
+									<button onclick={() => selectIndexer(indexer.ID.toString())}>
+										{indexer.Name} (ID: {indexer.ID})
+									</button>
+								</li>
+							{/each}
+						</ul>
+					{/if}
+				</div>
+			</div>
+
+			<div>
+				Path to file/folder/URL:
+				<input id="inputCollection" type="text" bind:value={collectionPath} placeholder="/user/example/folder-to-search" />
+				<button class="indexerButton" onclick={addCollection}>Add</button>
+			</div>
+			
+		</div>
 	</div>
 </main>
 
@@ -195,6 +309,7 @@
 		align-items: center;
 		gap: 0.5rem;
 		font-size: 1rem;
+		cursor: pointer;
 	}
 
 	.disabled-label {
@@ -227,11 +342,13 @@
 
 	input[type='range'] {
 		flex-grow: 1;
+		cursor: pointer;
 	}
 
 	input[type='checkbox'] {
 		accent-color: #517188;
 		transform: scale(1.5);
+		cursor: pointer;
 	}
 
 	/* Currently unused
@@ -250,7 +367,7 @@
 		margin-left: auto; /* optional: pushes input to far right */
 	}
 
-	#indexerButton {
+	.indexerButton {
 		padding: 0.65rem 1rem;
 		font-size: 1rem;
 		font-weight: 500;
@@ -318,5 +435,89 @@
 	#InputIndexer {
 		width: 350px;
 	}
+
+	.indexer-div {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		margin-top: 0.5rem;
+	}
+
+	#collectionDropdowns {
+		margin-bottom: 1.2rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		gap: 4rem;
+		width: 100%;
+	}
+
+	#collectionDropdowns .dropdown {
+		flex: 1;
+	}
+
+	.dropdown {
+		position: relative;
+		display: inline-block;
+		max-width: 300px;
+	}
+
+	.menu {
+		position: absolute;
+		background: #aec6df;
+		border: none;
+		border-radius: 4px;
+		margin-top: 4px;
+		list-style: none;
+		padding: 0;
+		width: 100%;
+	}
+
+	.menu li {
+		padding: 0;
+		margin: 0;
+		background: #aec6df;
+
+	}
+
+	.menu li button {
+		width: 100%;
+		padding: 8px 12px;
+		text-align: center;
+		border: none;
+		border-radius: 0;
+		cursor: pointer;
+	}
+	/*
+
+	.toggle {
+		padding: 8px 12px;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		background: white;
+		cursor: pointer;
+		min-width: 150px; 
+	}
+	*/
+
+	#inputCollection {
+		width: 280px;
+	}
+
+	.dropdownToggle {
+		width: 100%;
+		padding: 0.5rem;
+		cursor: pointer;
+		text-align: center;
+	}
+
+	.dropdown .dropdownToggle {
+		font-size: 1rem;
+	}
+
+	.dropdown .menu li button {
+		font-size: 1rem;
+	}	
 
 </style>
